8b42f61ff1ea410e9b0f78b279d5b6a09cc9594e
marammaryee1-jpg <maram.maryee1@gmail.com>
2026-01-09 11:47:04 -0500

    Implement functions for hollow square and patterns

--- Lab5.py
+++ Lab5.py
@@ -5,18 +5,48 @@
 # *   *
 # *****
 def hollow_square(n):
-    return ""
+    result = ""
+    i = 1
+
+    while i <= n:
+        if i == 1 or i == n:
+            result += "*" * n
+        else:
+            result += "*" + " " * (n - 2) + "*"
+
+        if i != n:
+            result += "\n"
+        i += 1
+    return result
 
 # 1
 # 12
 # 123
 # 1234
 def number_pattern(n):
-    return ""
+    result = ""
+    i = 1
+
+    while i <= n:
+        j = 1
+        while j <= i:
+            result += str(j)
+            j += 1
+
+        if i != n:
+            result += "\n"
+        i += 1
+    return result
 
 # Example: For n = 5, sum = 1 + 2 + 3 + 4 + 5 = 15
 def sum_of_natural_numbers(n):
-    return ""
+    total = 0
+    i = 1
+
+    while i <= n:
+        total += i
+        i += 1
+    return total
 
 # Example for n = 4:
 #    *
@@ -24,4 +54,15 @@ def sum_of_natural_numbers(n):
 #  *****
 # *******
 def centered_star_pyramid(n):
-    return ""
+    result = ""
+    i = 1
+    while i <= n:
+        spaces = n - i
+        stars = 2 * i - 1
+
+        result += " " * spaces + "*" * stars
+
+        if i != n:
+            result += "\n"
+        i += 1
+    return result
